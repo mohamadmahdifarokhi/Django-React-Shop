c91d64a0d54a662803145c60d9541952d26911b7
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-12-01 17:14:07 +0330

    Add product/admin.py | mti

--- ecommerce/apps/product/admin.py
+++ ecommerce/apps/product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.product.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'compare_price',
+                    'price', 'sold',)
+    list_display_links = ('id', 'name',)
+    list_filter = ('category',)
+    list_editable = ('compare_price', 'price')
+    search_fields = ('name', 'description',)
+    list_per_page = 25
