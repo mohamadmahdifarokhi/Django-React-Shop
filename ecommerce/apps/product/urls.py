8835910ee8fa6437999bdb5768eb103a23e55c6e
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-12-01 17:13:58 +0330

    Add product/urls.py | mti

--- ecommerce/apps/product/urls.py
+++ ecommerce/apps/product/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from .views import ProductDetailView, ListProductsView, ListSearchView, ListRelatedView, ListBySearchView
 
 app_name = 'product'
-ulrpatterns = [
-
+urlpatterns = [
+    path('product/<productId>', ProductDetailView.as_view()),
+    path('get-products', ListProductsView.as_view()),
+    path('search', ListSearchView.as_view()),
+    path('related/<productId>', ListRelatedView.as_view()),
+    path('by/search', ListBySearchView.as_view()),
 ]
