6671d6a8fb8d1cc26f7fe938f3335887c46f257e
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-12-02 19:12:57 +0330

    Add product models | mti

--- ecommerce/apps/product/models.py
+++ ecommerce/apps/product/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from datetime import datetime
 from apps.category.models import Category
 
+from django.conf import settings
+
+# domain = settings.DOMAIN
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/')
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     compare_price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    quality = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
     sold = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(default=datetime.now)
+
+    def get_thumbnail(self):
+        if self.photo:
+            return self.photo.url
+        return ''
 
     def __str__(self):
         return self.name
