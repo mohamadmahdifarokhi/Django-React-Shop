09e955cfb8d71b4a69c2a33976e1d36352549d31
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-12-11 10:16:59 +0330

    Add profile template

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -18,6 +18,7 @@ import Cart from "./containers/pages/Cart";
 import Checkout from "./containers/pages/Checkout";
 import ThankYou from "./containers/pages/ThankYou";
 import Dashboard from "./containers/pages/Dashboard";
+import DashboardProfile from "./containers/pages/DashboardProfile";
 
 function App() {
     return (
@@ -45,6 +46,9 @@ function App() {
                     <Route exact path='/thankyou' element={<ThankYou/>}/>
 
                     <Route exact path='/dashboard' element={<Dashboard/>}/>
+                    {/*<Route exact path='/dashboard/payments' element={<DashboardPayments/>}/>*/}
+                    {/*<Route exact path='/dashboard/payment/:transaction_id' element={<DashboardPaymentDetail/>}/>*/}
+                    <Route exact path='/dashboard/profile' element={<DashboardProfile/>}/>
                 </Routes>
             </Router>
         </Provider>
--- ecommerce/src/redux/reducers/index.js
+++ ecommerce/src/redux/reducers/index.js
@@ -8,6 +8,7 @@ import Shipping from "./shipping";
 import Payment from "./payment";
 import Coupons from "./coupons";
 import Orders from "./orders";
+import Profile from "./profile";
 
 export default combineReducers({
     Auth,
@@ -18,5 +19,6 @@ export default combineReducers({
     Shipping,
     Payment,
     Coupons,
-    Orders
+    Orders,
+    Profile
 })
