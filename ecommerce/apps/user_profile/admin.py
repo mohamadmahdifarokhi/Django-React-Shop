b7db7567486eb9663c9f7e2680ec47df236a2186
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-12-15 13:59:46 +0330

    Change userprofile admin

--- ecommerce/apps/user_profile/admin.py
+++ ecommerce/apps/user_profile/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import UserProfile, Address
 
-admin.site.register(UserProfile)
-admin.site.register(Address)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'address',
+                    'created', 'last_updated', 'deleted_at', 'restored_at', 'is_deleted', 'is_active')
+    list_filter = ('is_deleted', 'is_active')
+    search_fields = ('user__email', 'user__phone_number', 'phone')
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('body', 'city', 'created', 'last_updated', 'deleted_at', 'restored_at', 'is_deleted', 'is_active')
+    list_filter = ('is_deleted', 'is_active')
+    search_fields = ('body', 'city')
