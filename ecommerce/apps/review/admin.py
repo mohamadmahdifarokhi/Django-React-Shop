24863da2e7b8b3001c2c0fd6a86e8de19b4e9d1a
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-12-15 14:00:08 +0330

    Change shipping admin

--- ecommerce/apps/review/admin.py
+++ ecommerce/apps/review/admin.py
@@ -3,4 +3,9 @@ from .models import Review
 from ..core.admin import BaseAdmin
 
 
-admin.site.register(Review, BaseAdmin)
+@admin.register(Review)
+class ReviewAdmin(BaseAdmin):
+    list_display = ['user', 'product', 'head', 'body', 'rating', 'is_reply',
+                    'reply', 'created', 'last_updated', 'deleted_at', 'restored_at', 'is_deleted', 'is_active']
+    list_filter = ['user', 'product', 'is_reply']
+    search_fields = ['user', 'product', 'head', 'body', 'rating', 'is_reply', 'reply']
